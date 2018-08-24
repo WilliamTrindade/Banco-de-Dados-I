c9cc0012c86ce87b5f874a872cd3d44e19883406
William Trindade <williamtrindade777@gmail.com>
2018-08-24 14:17:08 -0300

    Update 1.sql

--- Banco-Estandes-Feira/1.sql
+++ Banco-Estandes-Feira/1.sql
@@ -1,5 +1,14 @@
 CREATE TABLE FISCAL
-	(codfis int,
-     nomefis varchar,
-     salario numeric(12,2),
-     primary key (codfis));
+	(	
+		codfis int,
+     	nomefis varchar,
+     	salario numeric(12,2),
+     	primary key (codfis)
+	);
+
+CREATE TABLE PRODUTO
+	(
+       	codproduto int,
+       	nomeproduto varchar(50),
+       	primary key(codproduto)
+	);
