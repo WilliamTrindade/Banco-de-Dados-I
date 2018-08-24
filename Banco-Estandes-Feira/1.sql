b24484e87ad50c796ef0e39a7b516946296ece9f
William Trindade <williamtrindade777@gmail.com>
2018-08-24 14:20:01 -0300

    Update 1.sql

--- Banco-Estandes-Feira/1.sql
+++ Banco-Estandes-Feira/1.sql
@@ -12,3 +12,13 @@ CREATE TABLE PRODUTO
        	nomeproduto varchar(50),
        	primary key(codproduto)
 	);
+	
+CREATE TABLE ESTANDE
+	(
+		codest int,
+        nomeest varchar(50),
+        aluguelest numeric(12,2),
+        codfis int,
+        primary key (codest),
+        foreign key (codfis) references fiscal(codfis)
+    );
