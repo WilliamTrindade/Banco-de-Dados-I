a3e772c93873a1236a4390b1ad41a8acbc704d63
William Trindade <williamtrindade777@gmail.com>
2018-08-24 15:00:45 -0300

    Create insert.sql

---
+++ Banco-Feira/insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO FISCAL(codfis, nomefis, salario) VALUES (1, 'jhon', '1000'), (2, 'Mary', '1000'), (3, 'Joaquim', '2000');
+INSERT INTO ESTANDE(codest, nomeest, aluguelest, codfis) VALUES (1, 'Estande1', '500', 1), (2, 'Estande2', '500', 3), (3, 'Estande3', '500', 1);
+INSERT INTO PRODUTO(codproduto, nomeproduto) VALUES (1, 'pastel'), (2, 'Torrada'), (3, 'Bauru'), (4, 'Croquete');
