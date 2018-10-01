c68589383c7db0b2a1e8c970dece7aebed833d2d
William Trindade <williamtrindade777@gmail.com>
2018-10-01 14:25:14 -0300

    Update 01.sql

--- atributos/atributos multivalorados/01.sql
+++ atributos/atributos multivalorados/01.sql
@@ -1,9 +1,9 @@
-#Se quisermos salvar vários números de telefone
-#Funcionário (codfunc, nomefunc) 
-#Telefone (codfunc, número) codfunc referencia Funcionário
+--Se quisermos salvar vários números de telefone--
+--Funcionário (codfunc, nomefunc) --
+--Telefone (codfunc, número) codfunc referencia Funcionário
 
-CREATE TABLE FUNCIONARIO (CODFUNC, NOMEFUNC, PRIMARY KEY(CODFUNC));
-CREATE TABLE TELEFONE (CODFUNC, NUMERO, FOREIGN KEY(CODFUNC) REFERENCES FUNCIONARIO);
-#telefone é entidade fraca
+CREATE TABLE FUNCIONARIO (CODFUNC INT, NOMEFUNC VARCHAR(50), PRIMARY KEY(CODFUNC));
+CREATE TABLE TELEFONE (CODFUNC INT, NUMERO VARCHAR(50), FOREIGN KEY(CODFUNC) REFERENCES FUNCIONARIO);
+--telefone é entidade frac--
                                                      
                                                         
