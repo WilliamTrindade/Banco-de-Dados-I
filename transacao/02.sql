d6a1543f711768e3962297cc5f18bf6fd94edf53
William Trindade <williamtrindade777@gmail.com>
2018-10-29 16:33:09 -0300

    Create 02.sql

---
+++ transacao/02.sql
@@ -0,0 +1,9 @@
+--entre o brgin e o commit ele executa sem erros--
+begin;
+
+UPDATE empregado SET salario = 999992 WHERE codemp = 1;
+SELECT IDSFIDIF;
+SELECT * FROM EMPREGADO;
+
+commit;
+SELECT * FROM empregado;
